c1fc05f06def151d9f31d3e800a3787c29993e59
webwings74 <rvdveldt@gmail.com>
2025-04-09 13:44:46 +0200

    Add example configuration file with API keys

---
+++ config-example.js
@@ -0,0 +1,5 @@
+const config = {
+    thunderforestApiKey: "Your Thunderforest API key",
+    tracestrackApiKey: "Your Tracestrack API key",
+    openRouteServiceApiKey: "Your OpenRouteService API key"
+};
